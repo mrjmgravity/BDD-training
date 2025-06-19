4483b2dcae63fa5057948c17673a39cd3a924599
mrjmgravity <sevenfold0198@gmail.com>
2025-06-19 18:34:58 +0200

    bank account added, and kalkulacka test added

--- src/main/java/KalkulackaTEst.java
+++ src/main/java/KalkulackaTEst.java
@@ -1,22 +1,30 @@
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class KalkulackaTEst {
 
+    int prvCislo;
+    int druCislo;
+    int vypoctenyVysledek;
+
     @Given("Uzivatel ma zadane dve cisla {int} a {int}")
-    public void uzivatelMaZadaneDveCislaA(int arg0, int arg1) {
+    public void uzivatelMaZadaneDveCislaA(int prvniCislo, int druheCislo) {
+        prvCislo = prvniCislo;
+        druCislo = druheCislo;
     }
 
     @When("Uzivatel vydeli tieto cisla")
     public void uzivatelVydeliTietoCisla() {
+
     }
 
-    @Then("Uzivatel vidi na kalkulacke vysledok {int}")
-    public void uzivatelVidiNaKalkulackeVysledok(int arg0) {
-    }
+   // @Then("Uzivatel vidi na kalkulacke vysledok {int}")
+   // public void uzivatelVidiNaKalkulackeVysledok(int predpokladanyVysledek) {
+     //    assertEquals(predpokladanyVysledek, vypoctenyVysledek);
+    //}
 
     @When("Uzivatel scita tieto cisla")
     public void uzivatelScitaTietoCisla() {
+        vypoctenyVysledek = prvCislo + druCislo;
     }
 }
